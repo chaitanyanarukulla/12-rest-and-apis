f45d802823c4f47d6253e6ecec1665e119286923
Chaitanya.Narukulla <Chai@Ashleys-MacBook-Air.local>
2017-07-25 17:49:50 -0700

    Getting DATA

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -11,6 +11,24 @@ var app = app || {};
     //       Remember that the callback function we'll want to call relies on repos.all
     //       being an array with a bunch of repo objects in it, so you'll need to
     //       populate it with the response from Github before you call the callback.
+    $.ajax({
+        url: 'https://api.github.com/user',
+        method: 'GET',
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`
+        }
+      })
+      .then(
+        data => {
+          console.log(data);
+          Object.keys(data).foreach(line => $('#results').append(
+            `<li>${line}: ${data[line]}</li>
+          `))
+        },
+        err => {
+          console.error(err)
+        }
+      )
 
   };
 
